44b07fd65ae5fd3512be89761a6781d9a66eb58d
Boyu1997 <jiangby97@gmail.com>
2019-04-05 02:40:06 -0300

    Init commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import sqlalchemy as db
+
+engine = db.create_engine('sqlite:///census.sqlite')
+connection = engine.connect()
+metadata = db.MetaData()
+census = db.Table('census', metadata, autoload=True, autoload_with=engine)
